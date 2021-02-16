46742b7dfc044d2d17c67c26b48e0b06ef18e1a9
MkhCoder <manukharbanda@gmail.com>
2021-02-16 11:08:02 -0500

    Initial

--- src/main/buildsomething/KotlinApp.kt
+++ src/main/buildsomething/KotlinApp.kt
@@ -1,12 +1,33 @@
-package main.kotlin.com.buildsomething
+package com.buildsomething
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 
 @SpringBootApplication
-open class KotlinApp
+class KotlinApp
 
 fun main(args: Array<String>){
     SpringApplication.run(KotlinApp::class.java, *args)
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+    @GetMapping()
+    fun hello(): ResponseEntity<String> {
+        return ResponseEntity.ok("Hello World")
+    }
+
+    @GetMapping(path = ["/{name}"])
+    fun helloWorldParam(@PathVariable name: String) : ResponseEntity<String> {
+        return ResponseEntity.ok("Hello $name")
+    }
+
+
 }
